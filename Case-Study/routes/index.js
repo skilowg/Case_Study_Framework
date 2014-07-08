56c3bdd8f22dc667eb2970d6688f1e4d199fabd1
skilowg <skilowg@gmail.com>
2014-07-07 23:38:58 -0700

    Test styles and file links

--- Case-Study/routes/index.js
+++ Case-Study/routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'party' });
+});
+
+/* GET Hello World page. */
+router.get('/helloworld', function(req, res) {
+    res.render('helloworld', { title: 'Hello, World!' });
 });
 
 module.exports = router;
